4e452152fa3b39115b1011d20f2eb46777875a00
ravikirankiran097 <55925733+ravikirankiran097@users.noreply.github.com>
2022-12-28 13:18:45 +0530

    update

--- save_to_dvc.py
+++ save_to_dvc.py
@@ -15,7 +15,7 @@ VECTOR = meta_data["VECTOR_PATH"]
 MODEL = meta_data["MODEL_PATH"]
 
 # Configure DVC
-os.system("dvc remote add origin {DVC_REMOTE_URL}")
+os.system("dvc remote add -f origin {DVC_REMOTE_URL}")
 os.system("dvc remote modify origin --local auth basic")
 os.system("dvc remote modify origin --local user {USERNAME}}")
 os.system("dvc remote modify origin --local password {PASSWORD}")
